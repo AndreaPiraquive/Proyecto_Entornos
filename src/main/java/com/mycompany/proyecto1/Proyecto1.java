6f8c14e02e2afc6aaee8106be678adb414bd8dc7
Lu Piraquive <Lu Piraquive@DESKTOP-TBL9PMD>
2025-01-12 17:15:41 +0100

    prueba proyecto entornos

---
+++ src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1;
+
+/**
+ *
+ * @author Lu Piraquive
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("esta es la practica de Entornos ");
+    }
+}
