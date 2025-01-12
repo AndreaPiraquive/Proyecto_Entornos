b836b5ea51d05380bd80aeb3b5da6ad70b2a83f1
Rocio1604 <rocpago@gmail.com>
2025-01-12 17:49:53 +0100

    Create Persona.java

---
+++ src/main/java/com/mycompany/proyecto_entornos/modelo/Persona.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_entornos;
+
+/**
+ *
+ * @author rocki
+ * @version 1.1.1
+ */
+public class Persona {
+    public void hola(){
+        for(int i=0; i<5; i++){
+            System.out.println("Hola , ¿que tal?");
+        }
+    }
+}
